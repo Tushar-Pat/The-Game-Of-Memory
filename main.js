531795058db74addd88d07b4e74308dafbe0edaa
Tushar Patel <100702393@oncampus.local>
2020-05-05 00:50:42 -0400

    Adding game types

--- main.js
+++ main.js
@@ -1,20 +1,25 @@
 var userInput = "";
 var output = "yoinks";
 var counter = 0;
-var arr = new Array("red", "green", "blue", "orange", "yellow", "purple");
-//var arr = new Array(6);
+//var arrLength = 0;
+var colours = new Array("red", "green", "blue", "orange", "yellow", "purple"); //this is gonna be colours
+var arr = Object.assign([],colours);
+var cities = new Array("Toronto", "Vancouver", "Oshawa", "Montreal", "Calgary", "Hamilton");
+var countries = new Array("Canada", "USA", "India", "Mexico", "China", "France");
+
+//Main Game
 
 function execute() {
     userInput = document.getElementById("textInput").value;
 
-    removeIndex()
+    removeIndex();
     findNewValue();
 
     document.getElementById("output").innerHTML = output;
 }
 
 function removeIndex() {
-    if (counter == 3) {
+    if (counter == 6) {
         window.alert("You win!!!");
     } else {
         var valueExists = false;
@@ -30,17 +35,40 @@ function removeIndex() {
             window.alert("You lose :(");
         }
 
-        counter += 1;
+        counter+=1;
     }
 }
 
 function findNewValue() {
     output = arr[0];
     arr.splice(0, 1);
+    counter+=1;
 }
 
-//game type commands
+//game type commands (need to get working)
 
 function colours(){
-    arr = colours;
+    //arr = Object.assign([], colours);
+
+    //arrLength = arr.length;
+
+    document.getElementById("output").innerHTML = "yoinks";
+}
+
+function cities(){
+    counter = 0;
+    for(var x = 0; x < arr.length; x++){
+        arr[x] = cities[x]
+    }
+
+    arrLength = cities.length;
+}
+
+function countries(){
+    counter = 0;
+    for(var x = 0; x < arr.length; x++){
+        arr[x] = countries[x]
+    }
+
+    arrLength = countries.length;
 }
